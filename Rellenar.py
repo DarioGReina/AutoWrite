489dfcf86387db868a44b8f4ab0cd9d38b36e1c1
Daniel Gonzalez Reina <dangonrei@gmail.com>
2020-07-27 20:22:13 +0200

    Improvements in Rellenar.py

--- Rellenar.py
+++ Rellenar.py
@@ -1,30 +1,27 @@
 scriptPath = nuke.root().knob('name').value()
-scriptName = ((scriptPath.split('/'))[(len((scriptPath.split('/')))-1)]).split('.')[0]
+scriptName = scriptPath.split('/')[-1].split('.')[0]
 
-cap = scriptName.split('_')[0]
-sec = scriptName.split('_')[1]
-plano = scriptName.split('_')[2]
-dep = scriptName.split('_')[3]
-op = scriptName.split('_')[4]
-vCliente = (scriptName.split('_')[5])[1]
-vOp = ((scriptName.split('_')[5])[2]) + ((scriptName.split('_')[5])[3])
+[cap, sec, plano, dep, op, version] = scriptName.split('_')
+vCliente = version[1]
+vCliente = scriptNameSections[5][1]
+vOp = version[2:]
 
-nuke.thisNode()['cap'].setValue(cap)
-nuke.thisNode()['sec'].setValue(sec)
-nuke.thisNode()['plano'].setValue(plano)
-nuke.thisNode()['dep'].setValue(dep)
-nuke.thisNode()['op'].setValue(op)
-nuke.thisNode()['vCliente'].setValue(vCliente)
-nuke.thisNode()['vOp'].setValue(vOp)
-nuke.thisNode()['scriptName'].setValue(scriptName)
+nukeNode = nuke.thisNode()
+nukeNode['cap'].setValue(cap)
+nukeNode['sec'].setValue(sec)
+nukeNode['plano'].setValue(plano)
+nukeNode['dep'].setValue(dep)
+nukeNode['op'].setValue(op)
+nukeNode['vCliente'].setValue(vCliente)
+nukeNode['vOp'].setValue(vOp)
+nukeNode['scriptName'].setValue(scriptName)
 
-pixAspect = nuke.thisNode()['pixAspect'].getValue()
-pixAspRnd = float(int(pixAspect*100))/100
+pixAspect = nukeNode['pixAspect'].getValue()
+pixAspRnd = round(pixAspect, 2)
 
 if pixAspect == 1:
-   nuke.thisNode()['pixAspectTxt'].setValue('Square Pixel')
+  nukeNode['pixAspectTxt'].setValue('Square Pixel')
+elif pixAspect == 2:
+  nukeNode['pixAspectTxt'].setValue('Anamorphic')
 else:
-   if pixAspect == 2:
-      nuke.thisNode()['pixAspectTxt'].setValue('Anamorphic')
-   else:
-      nuke.thisNode()['pixAspectTxt'].setValue(str(pixAspRnd)+':1')
+  nukeNode['pixAspectTxt'].setValue(str(pixAspRnd) + ':1')
